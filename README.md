bcf634fe9e5dcc1348dfe415a3739cb704b64f6a
Björn Blissing <bjorn.blissing@vti.se>
2014-08-17 01:12:23 +0200

    License information added.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ License
 Source code is licensed according to the 3-clause license BSD-license. 
 See license.txt for complete licensing information. 
 
+**NOTE** By linking to the Oculus SDK libraries you must adhere to the terms of the Oculus SDK License Agreement.
+
 
 Contributors
 ------------
--- cmake/FindOculusSDK.cmake
+++ cmake/FindOculusSDK.cmake
@@ -22,21 +22,21 @@ FIND_PATH(OCULUS_SDK_INCLUDE_DIRS NAMES OVR.h HINTS
 # Determine architecture
 IF(CMAKE_SIZEOF_VOID_P MATCHES "8")
 	IF(UNIX)
-		SET(OCULUS_SDK_LIB_ARCH "x86_64" CACHE STRING "library location")
+		SET(_OCULUS_SDK_LIB_ARCH "x86_64")
 	ENDIF()
 	IF(MSVC)
-		SET(OCULUS_SDK_LIB_ARCH "x64" CACHE STRING "library location")
+		SET(_OCULUS_SDK_LIB_ARCH "x64")
 	ENDIF()
 ELSE()
 	IF(UNIX)
-		SET(OCULUS_SDK_LIB_ARCH "i386" CACHE STRING "library location")
+		SET(_OCULUS_SDK_LIB_ARCH "i386")
 	ENDIF()
 	IF(MSVC)
-		SET(OCULUS_SDK_LIB_ARCH "Win32" CACHE STRING "library location")
+		SET(_OCULUS_SDK_LIB_ARCH "Win32")
 	ENDIF()
 ENDIF()
 
-MARK_AS_ADVANCED(OCULUS_SDK_LIB_ARCH)
+MARK_AS_ADVANCED(_OCULUS_SDK_LIB_ARCH)
 
 # Append "d" to debug libs on windows platform
 IF (WIN32)
@@ -47,18 +47,17 @@ ENDIF()
 IF (MSVC)
 	# Visual Studio 2010
 	IF(MSVC10)
-		SET(OCULUS_MSVC_DIR "VS2010" CACHE STRING "library location")
+		SET(_OCULUS_MSVC_DIR "VS2010")
 	ENDIF()
 	# Visual Studio 2012
 	IF(MSVC11)
-		SET(OCULUS_MSVC_DIR "VS2012" CACHE STRING "library location")
+		SET(_OCULUS_MSVC_DIR "VS2012")
 	ENDIF()
 	# Visual Studio 2013
 	IF(MSVC12)
-		SET(OCULUS_MSVC_DIR "VS2013" CACHE STRING "library location")
+		SET(_OCULUS_MSVC_DIR "VS2013")
 	ENDIF()
 ENDIF()
-MARK_AS_ADVANCED(OCULUS_MSVC_DIR)
 
 # Try to ascertain the version of the SDK
 IF(OCULUS_SDK_INCLUDE_DIRS)
@@ -75,22 +74,27 @@ IF(OCULUS_SDK_INCLUDE_DIRS)
 	ENDIF()
 ENDIF()
 
+# Locate Oculus license file
+SET(_OCULUS_SDK_LICENSE_FILE "${OCULUS_SDK_ROOT_DIR}/LICENSE.txt")
+IF(EXISTS "${_OCULUS_SDK_LICENSE_FILE}") 
+	SET(OCULUS_SDK_LICENSE_FILE "${_OCULUS_SDK_LICENSE_FILE}" CACHE INTERNAL "The location of the Oculus SDK license file")
+ENDIF()
 
 # Look for the library.
 FIND_LIBRARY(OCULUS_SDK_LIBRARY NAMES libovr libovr64 ovr HINTS ${OCULUS_SDK_ROOT_DIR} 
-                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/${OCULUS_SDK_LIB_ARCH}/${OCULUS_MSVC_DIR}
+                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/${_OCULUS_SDK_LIB_ARCH}/${_OCULUS_MSVC_DIR}
                                                       ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Mac/Release
-                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Release/${OCULUS_SDK_LIB_ARCH}
+                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Release/${_OCULUS_SDK_LIB_ARCH}
                                                     )
 
 # This will find release lib on Linux if no debug is available - on Linux this is no problem and avoids 
 # having to compile in debug when not needed
 FIND_LIBRARY(OCULUS_SDK_LIBRARY_DEBUG NAMES libovr${CMAKE_DEBUG_POSTFIX} libovr64${CMAKE_DEBUG_POSTFIX} ovr${CMAKE_DEBUG_POSTFIX} ovr libovr HINTS 
-                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/${OCULUS_SDK_LIB_ARCH}/${OCULUS_MSVC_DIR}
+                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/${_OCULUS_SDK_LIB_ARCH}/${_OCULUS_MSVC_DIR}
                                                       ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Mac/Debug
                                                       ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Mac/Release
-                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Debug/${OCULUS_SDK_LIB_ARCH}
-                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Release/${OCULUS_SDK_LIB_ARCH}
+                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Debug/${_OCULUS_SDK_LIB_ARCH}
+                                                      ${OCULUS_SDK_ROOT_DIR}/LibOVR/Lib/Linux/Release/${_OCULUS_SDK_LIB_ARCH}
                                                     )
     
 MARK_AS_ADVANCED(OCULUS_SDK_LIBRARY)
