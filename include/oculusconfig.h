3db7af19c25901cf88d9c0e24633b77a583218dc
Riccardo Corsi <riccardo.corsi@kairos3d.it>
2020-12-30 11:19:23 +0100

    STUB: added option to build the osgOculus lib as dynamic

--- include/oculusconfig.h
+++ include/oculusconfig.h
@@ -7,8 +7,17 @@
 
 #pragma once
 
-//#define OSGOCULUS_EXPORT __declspec( dllexport )
-//#define OSGOCULUS_EXPORT __declspec( dllimport )
+
+// define the dll export directives based on CMake option
+#ifdef OSGOCULUSLIB_BUILD_SHARED
+#  if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
+#     ifdef OsgOculus_EXPORTS
+#        define OSGOCULUS_EXPORT __declspec(dllexport)
+#     else
+#        define OSGOCULUS_EXPORT __declspec(dllimport)
+#     endif
+#  endif
+#endif
 
 #ifndef OSGOCULUS_EXPORT
 #define OSGOCULUS_EXPORT
